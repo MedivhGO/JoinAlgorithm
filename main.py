f81ca2ae0931cfee723f2cc6b05ff17352453e70
MedivhGO <lijingqi93@gmail.com>
2021-06-22 19:34:15 +0800

    finish structer

---
+++ main.py
@@ -0,0 +1,37 @@
+import timeit
+
+
+def nested_join(r, s):
+    res = []
+    for x in r:
+        for y in s:
+            res.append([x, y])
+
+
+def sort_merge_join(r, s):
+    pass
+
+
+def hash_join(r, s):
+    pass
+
+
+if __name__ == '__main__':
+    r = []
+    s = []
+    for i in range(1, 5):
+        print("testcase"+str(i))
+        print("-------------------------")
+        with open("./testcase" + str(i),'r') as f:
+            r = f.readline().split(',')
+            s = f.readline().split(',')
+            t1 = timeit.Timer('nested_join(r,s)', 'from __main__ import nested_join, r, s')
+            t2 = timeit.Timer('sort_merge_join(r, s)', 'from __main__ import sort_merge_join, r, s')
+            t3 = timeit.Timer('hash_join(r, s)', 'from __main__ import hash_join, r, s')
+            print("run nested join time : ")
+            print(t1.timeit())
+            print("run sort merge join time: ")
+            print(t2.timeit())
+            print("run hash join time: ")
+            print(t3.timeit())
+        print("-------------------------")
